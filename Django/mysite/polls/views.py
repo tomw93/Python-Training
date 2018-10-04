a170fb9dd5efb97ae68123490b8ed8312c6bd05f
Tom Wilkins <tom.wilkins@citrix.com>
2018-10-04 09:59:30 +0100

    Finished django starter tutorial + added docker todo app example

--- Django/mysite/polls/views.py
+++ Django/mysite/polls/views.py
@@ -1,23 +1,47 @@
-from django.http import HttpResponse
-from django.http import Http404
-from django.template import loader
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list':latest_question_list
-    }
-    return render(request, 'polls/index.html', context)
+from .models import Choice,Question
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    # we don't need to provide this for the other views as it's
+    # implied from the 'model'
+    context_object_name = 'latest_question_list'
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    def get_queryset(self):
+        """return the last five published questions"""
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    # generic.DetailView expects the primary key value captured from the URL to be 
+    # called pk, which is why we changed question_id to pk
+    model = Question
+    template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message':"You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
